f521e771e1936f9276ac502782324047b4adbfa7
Stragroid <stragroid@gmail.com>
2022-12-05 22:39:03 +0530

    Minor fixes for production

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-// import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
 
 import App from './App'
 import './index.css'
@@ -10,12 +8,9 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
-  // <QueryClientProvider client={queryClient}>
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      {/* <ReactQueryDevtools initialIsOpen /> */}
     </BrowserRouter>
   </React.StrictMode>
-  // </QueryClientProvider>
 )
